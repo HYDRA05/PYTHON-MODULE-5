b928ac080982530307bdf26b437355053d39d7fc
HYDRA05 <tanushreesahoo720@gmail.com>
2025-08-11 16:16:55 +0530

    Add task1.py for assignment

--- task1.py
+++ task1.py
@@ -1,10 +1,10 @@
-Task 1: Read a File and Handle Errors 
-Problem Statement:  Write a Python program that:
-1.   Opens and reads a text file named sample.txt.
-2.   Prints its content line by line.
-3.   Handles errors gracefully if the file does not exist.
+# Task 1: Read a File and Handle Errors 
+# Problem Statement:  Write a Python program that:
+# 1.   Opens and reads a text file named sample.txt.
+# 2.   Prints its content line by line.
+# 3.   Handles errors gracefully if the file does not exist.
  
-file1=open ('sample.txt','r')
+file1=open ('sample.txt.py','r')
 reading_file1=file1.read()
 print(reading_file1)
-file.close()
+file1.close()
